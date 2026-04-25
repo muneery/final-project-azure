56e474e0b562e3649d7b6040349ad8dc1cdd2692
Muneer Al-Khasawneh <112361385+muneery@users.noreply.github.com>
2026-04-25 01:42:07 -0400

    Update home route response message

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello Azure!"
+    return "Azure Flask App is working!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
