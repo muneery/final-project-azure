492b261b152844e54e903ebd0fd296e2f7a0d246
Muneer Al-Khasawneh <112361385+muneery@users.noreply.github.com>
2026-04-25 03:02:14 -0400

    Normalize column names and update comments
    Updated comments for clarity and normalized column names.

--- data_loader.py
+++ data_loader.py
@@ -8,11 +8,12 @@ def load_data():
     transactions = pd.read_csv(os.path.join(BASE_DIR, "400_transactions.csv"))
     products = pd.read_csv(os.path.join(BASE_DIR, "400_products.csv"))
 
-    # FIX column names to lowercase
+    # normalize columns
     households.columns = households.columns.str.lower()
     transactions.columns = transactions.columns.str.lower()
     products.columns = products.columns.str.lower()
 
+    # merge
     df = transactions.merge(products, on="product_num", how="left")
     df = df.merge(households, on="hshd_num", how="left")
 
